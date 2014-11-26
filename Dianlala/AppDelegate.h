fecd0ea6e08a2d5c8ea98a3bd52eea70e3094530
mexiqq <lijianwei@139.me>
2014-11-26 15:39:21 +0800

    init project

---
+++ Dianlala/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dianlala
+//
+//  Created by MexiQQ on 14/11/26.
+//  Copyright (c) 2014年 MexiQQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dianlala/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dianlala
+//
+//  Created by MexiQQ on 14/11/26.
+//  Copyright (c) 2014年 MexiQQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
